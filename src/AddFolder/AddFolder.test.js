8b8760cfc6833012c09e104346a5267c0e9631d4
Grace <mgracebodur@gmail.com>
2019-11-29 00:28:29 -0500

    Adding tests to AddNotes and AddFolders

---
+++ src/AddFolder/AddFolder.test.js
@@ -0,0 +1,6 @@
+// import React from 'react'
+// import {shallow} from 'enzyme'
+// import {toJson} from 'enzyme-to-json'
+// import AddFolder from './AddFolder'
+
+// it(`renders without crashing`, )
